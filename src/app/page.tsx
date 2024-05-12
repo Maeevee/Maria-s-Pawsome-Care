cbdcd45989d587dfa07e5b051e919fbda729722e
Mariia Kiriakulova <mariya.kiryakulova@gmail.com>
2024-05-12 21:54:39 +0300

    set-timer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client"
-// import { HomePage } from "@/features/HomePage/HomePage";
-// import Image from "next/image";
+import { HomePage } from "@/features/HomePage/HomePage";
+import Image from "next/image";
 import { useEffect } from 'react';
 
 export default function Home() {
 
   useEffect(() => {
     // Redirect the user to direct.me/mariaspawcare
-    window.location.href = 'https://direct.me/mariaspawcare';
+    setTimeout(() => window.location.href = 'https://direct.me/mariaspawcare', 5000)
   }, []);
 
   // This component doesn't need to render anything since the redirection happens immediately
